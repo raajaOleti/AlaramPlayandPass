8be8ea88fb1dc306a07e25daa8052fc1f34c252f
unknown <raajaoleto07@gmail.cpm>
2024-03-24 18:07:59 +0530

    alaram

---
+++ Alaram.js
@@ -0,0 +1,9 @@
+var aud = new Audio();
+console.log(aud);
+aud.src="../AlaramPlay_Pass/assets/audio/tropical-alarm-clock-168821.mp3"
+function ply(){
+    aud.play();
+}
+function stopAlm(){
+    aud.pause();
+}
